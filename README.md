f23850d4e16c5ad1a5da867edd97907a6b94bc86
Alanduril <mariusz.stachniuk@gmail.com>
2023-06-15 16:56:06 +0200

    Adding code and readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Winner-identifier
+Tic Tac Toe winner identifier
+
+#########################################
+
+Please, avoid changing the source code for the exercise!
+
+#########################################
+
+
+Soon, you will try to create your own Tic Tac Toe game, so let's prepare a bit!
+
+
+This time, you have to write only a checker for the game. It will be a method that takes a 2D array and returns a boolean. If there is a winner, it returns true otherwise false.
+
+
+We use "X" and "O" as signs of our players. The empty places will be filled with numbers from 1 to 9.
+
+You have to check 3 types of cases:
+
+    horizontal;
+
+    vertical;
+
+    diagonal;
+
+Hint: you can use more than one condition inside of one if, and don't hesitate to use loop;
---
+++ TicTacToe_Winner_Identifier_Challenge/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TicTacToe_Winner_Identifier_Challenge
+{
+    class Program
+    {
+        public static string[,] board = new string[3, 3]
+            {
+                {"1","2","3" },
+                {"4","5","6" },
+                {"7","8","9" }
+            };
+        public static int counter;
+        static void Main(string[] args)
+        {
+            //horizontal a
+            //for (int i = 0,j=0; i < board.GetLength(0); i++,j++)
+            //{
+            //    if(i==j)
+            //    {
+            //        board[i, j] = "x";
+            //    }
+            //}
+
+            //horizontal b
+            //for (int i =0, j=2; i<board.GetLength(0);i++,j--)
+            //{
+            //    board[i, j] = "x";
+            //}
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(0); j++)
+                {
+                    // change i for vertical or j for horizontal lines
+                    //board[i, 2] = "x";
+                    Console.Write(board[i, j] + " ");
+                    if (j == 2)
+                    {
+                        Console.Write("\n");
+                    }
+                    if (board[i, j] == "x")
+                    {
+                        counter++;
+                    }
+                }
+                
+            }
+            if (counter == 3)
+            {
+                Console.WriteLine("winner");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
